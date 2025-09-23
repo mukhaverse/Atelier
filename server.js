a41b50dcaaaa8c0657be4182ad6a90277c6971b0
Shumokh <shumokhalsharif@gmail.com>
2025-09-23 23:10:46 +0300

    basic server structure

--- server.js
+++ server.js
@@ -1,4 +1,53 @@
 /**
- * this will be the backeend server
+ * these lines are for Ghaida for any database code
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  * 
  */
+
+const express = require("express")
+
+const app = express()
+
+app.get("/products", (req, res) =>{
+    res.send("this will fetch all products")
+})
+
+app.get("/products/:category", (req, res) =>{
+    let category = req.params.category
+    res.send(`this will fetch products from the ${category} category`)
+})
+
+app.get("/product/id", (req, res) =>{
+    res.send("this will fetch a single product")
+})
+
+
+app.listen(3000, () =>{
+    console.log("I'm listening in the port 3000")
+})
