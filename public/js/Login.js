edd6bebb0a09a18c4fa287e2510d003fd9f34a31
Shumokh <shumokhalsharif@gmail.com>
2025-11-06 04:44:52 +0300

    add sign up and finished animation

--- public/js/Login.js
+++ public/js/Login.js
@@ -1,83 +1,72 @@
 const shape = document.querySelector('.shape');
 const logLeft = document.querySelector('.log-div');
+const signRight = document.querySelector('.sign-div')
 
 // const contentRight = document.querySelector('.content-right');
 // const btnLeft = document.getElementById('btn-left');
 // const btnRight = document.getElementById('btn-right');
 
 const registerBtn = document.querySelector('.reg-link') 
+const loginBtn = document.querySelector('.log-link')
 
-const timeline1 = gsap.timeline()
-
-// registerBtn.addEventListener("click", () =>{
-//     registerBtn.style.color = "red";
-    
-// })
-
-registerBtn.addEventListener("click", (e) =>{
-    registerBtn.classList.add('active');
-
-
-
-//                     animation for disktop
+const timeline1 = gsap.timeline({ paused: true })
 
 const larg = window.matchMedia("(max-width: 1700px)");
-if (larg.matches) {
+const mid = window.matchMedia("(max-width: 768px)");
+const small = window.matchMedia("(max-width: 480px)");
+
+
+
+if (small.matches) {
+  console.log("Viewport is 480px or smaller.");
+
+
+  timeline1.to(shape, { scale: 4, duration: 1, ease: 'cric'})
+            .to(logLeft, { opacity: 0, duration: 0.1 })
+            .to(signRight, {opacity: 1, duration: 0.1})
+            .to(shape,{  x: '-105%', scale: 1,y: '-170%' , duration: 1, ease: 'cric' })
+  
+
+
+} else if (mid.matches) {
+    console.log("Medium screen (≤768px)");
+
+     timeline1.to(shape, { scale: 4.3, duration: 1, ease: 'cric'})
+            .to(logLeft, { opacity: 0, duration: 0.3 })
+            .to(signRight, {opacity: 1, duration: 0.3})
+            .to(shape,{  x: '-140%', scale: 1,y: '-145%' , duration: 1, ease: 'cric' })
+    
+
+
+
+  } else if (larg.matches) {
   console.log("Viewport is 1700px or smaller.");
 
 
-  timeline1.to(logLeft, { opacity: 0, x: -30, duration: 1 })
-            .to(shape, { 
-            scale: 3, 
-            duration: 1,
-            ease: 'crick'
-        })
-            .to(shape,{ 
-                x: '-100%',
-            scale: 1,
-            // y: '-300%' ,
-            duration: 1, 
-            ease: 'power2.inOut' 
-            })
+  timeline1.to(shape, { scale: 3,  duration: 1, ease: 'cric' })
+            .to(logLeft, { opacity: 0, duration: 0.1 })
+            .to(signRight, {opacity: 1, duration: 0.1})
+            .to(shape,{  x: '-100%',scale: 1, duration: 1,  ease: 'cric' })
   
 
-
-} else {
-  console.log("Viewport is larger than 1700px.");
-}
+} 
 
 
 
+registerBtn.addEventListener("click", () =>{
+    registerBtn.classList.add('active');
 
-// const mid = window.matchMedia("(max-width: 768px)");
-// if (mid.matches) {
-//   console.log("Viewport is 768px or smaller.");
+    timeline1.play(); 
 
+    signRight.style.pointerEvents = "auto"
+    logLeft.style.pointerEvents = "none"
 
-//   timeline1.to(logLeft, { opacity: 0, x: 30, duration: 1 })
-//             .to(shape, { 
-//             scale: 3.5,
-//             duration: 1,
-//             ease: 'crick'
-//         })
-//             .to(shape,{ 
-//             x: '-130%',
-//             scale: 1,
-//             y: '-130%' ,
-//             duration: 1, 
-//             ease: 'power2.inOut' 
-//             })
-  
+})
 
-
-
-
-// } else {
-//   console.log("Viewport is larger than 768px.");
-// }
-
-
-
+loginBtn.addEventListener("click", () => {
+    timeline1.reverse(); 
+    logLeft.style.pointerEvents = "auto"
+    signRight.style.pointerEvents = "none"
 })
 
 
@@ -92,15 +81,13 @@ if (larg.matches) {
 
 
 
+// const small = window.matchMedia("(max-width: 480px)");
 
-
-const small = window.matchMedia("(max-width: 480px)");
-
-if (small.matches) {
-  console.log("Viewport is 480px or smaller.");
-} else {
-  console.log("Viewport is larger than 480px.");
-}
+// if (small.matches) {
+//   console.log("Viewport is 480px or smaller.");
+// } else {
+//   console.log("Viewport is larger than 480px.");
+// }
 
 
 
