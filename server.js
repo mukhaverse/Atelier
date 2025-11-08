65f2350f04afefd129120efc59beb949a66ea303
Shumokh <shumokhalsharif@gmail.com>
2025-11-08 23:39:28 +0300

    trying to log the products endpoint error

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.get("/products", async (req, res) =>{
     res.json(products)
     return
   }catch (error) {
-    console.log("error while fetching products ")
-    return res.send("error ", error)
+    console.log("error while fetching products ", error)
+    return res.send("error ")
   }
 
     
